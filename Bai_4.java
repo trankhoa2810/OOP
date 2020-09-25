d1cfa7e0d348461dde239a96a8fc5767211fb037
Khoa Tran <55272957+trankhoa2810@users.noreply.github.com>
2020-09-25 09:48:04 +0700

    Add files via upload

---
+++ Bai_4.java
@@ -0,0 +1,35 @@
+package buoi_1;
+import java.util.Scanner;
+
+public class Bai_4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A = Integer.MAX_VALUE;
+		long B = Integer.MAX_VALUE;
+		String str = "";
+		while(A == Integer.MAX_VALUE)
+		{
+			try {
+				System.out.println("Nhap a = ");
+				str = sc.nextLine();
+				A = Long.parseLong(str);
+			}
+			catch(Exception e){
+				A = Integer.MAX_VALUE;
+			}
+		}
+		
+		while(B == Integer.MAX_VALUE)
+		{
+			try {
+				System.out.println("Nhap b = ");
+				str = sc.nextLine();
+				B = Long.parseLong(str);
+			}
+			catch(Exception e){
+				B = Integer.MAX_VALUE;
+			}
+		}
+		System.out.println(A+B);
+	}
+}
